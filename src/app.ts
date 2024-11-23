a58c6a85f4f570f4de8c07991d198adb1196d3f6
Prateek Patidar <patidarprateek2593@gmail.com>
2024-11-23 22:47:36 +0530

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express from "express";
+import globalErrorHannler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 // Routes
-app.get('/', (req, res, next) => {
-    res.json({message:"Hello APIs!"});
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello APIs!" });
+});
 
+// global error handler
+app.use(globalErrorHannler);
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,11 @@
-import {config as conf} from 'dotenv';
+import { config as conf } from "dotenv";
 conf();
 
 const _config = {
-    port:process.env.PORT,
-    databaseUrl: process.env.MONGO_CONNECTION_STRING
-}
+  port: process.env.PORT,
+  databaseUrl: process.env.MONGO_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
+};
 
 //Object.freeze() - Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
-export const config = Object.freeze(_config)
+export const config = Object.freeze(_config);
