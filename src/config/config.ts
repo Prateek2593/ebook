0e3c7dce91e45e95146fe4a8b3d3a8ed5f3265b9
Prateek Patidar <patidarprateek2593@gmail.com>
2024-11-24 22:53:08 +0530

    upload file to cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const _config = {
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
+  cloudinaryKey: process.env.CLOUDINARY_API_KEY,
+  cloudinarySecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 //Object.freeze() - Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
