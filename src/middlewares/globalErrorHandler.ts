782b6815f4b6d2c1c8228cb985d3aa9acbdaa399
Prateek Patidar <patidarprateek2593@gmail.com>
2024-11-24 16:22:37 +0530

    fixed globalErrorHandler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import { config } from "../config/config";
 
 //Glocal error handlers
-const globalErrorHannler = (
+const globalErrorHannler: ErrorRequestHandler = (
   err: HttpError,
   req: Request,
   res: Response,
@@ -11,7 +11,7 @@ const globalErrorHannler = (
 ) => {
   const statusCode = err.statusCode || 500;
 
-  return res.status(statusCode).json({
+  res.status(statusCode).json({
     message: err.message,
     errorStack: config.env === "development" ? err.stack : "",
   });
